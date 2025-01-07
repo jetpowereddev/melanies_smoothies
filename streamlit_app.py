25c2b4d27f3aadafe799c26ed5b275af0788a994
jetpowereddev <graham.morrow@jet2.com>
2025-01-07 09:08:17 +0000

    Update streamlit_app.py
    Debugging dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -34,6 +35,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
 
     #st.write(ingredients_string)
 
